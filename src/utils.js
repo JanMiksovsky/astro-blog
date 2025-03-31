39f6f85a3ac94883599d3e58fefeab3fe782759f
Jan Miksovsky <jan@miksovsky.com>
2025-03-31 15:30:14 -0700

    More work

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 // Return the array of posts
 // These are the post imports plus other data we care about
 export async function allPosts() {
-  const globs = import.meta.glob("../posts/*.md");
+  const globs = import.meta.glob("../markdown/*.md");
   return Promise.all(
     Object.entries(globs).map(async ([path, resolver]) => {
       const post = await resolver();
