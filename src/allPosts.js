48f12739493c18084842ddbd0fb13b36750103f6
Jan Miksovsky <jan@miksovsky.com>
2025-04-07 09:51:19 -0700

    Tweak

--- src/allPosts.js
+++ src/allPosts.js
@@ -1,5 +1,6 @@
-// Return the array of posts
-// These are the post imports plus other data we care about
+import parseDate from "./parseDate.js";
+
+// Return the array of posts: the post import plus other data we care about
 export default async function allPosts() {
   const globs = import.meta.glob("../markdown/*.md");
   const posts = await Promise.all(
@@ -7,7 +8,7 @@ export default async function allPosts() {
       const post = await resolver();
       const fileName = path.split("/").pop();
       const slug = fileName.replace(/.md$/, "");
-      const date = new Date(slug);
+      const date = parseDate(slug);
       return Object.assign({}, post, {
         date,
         slug,
